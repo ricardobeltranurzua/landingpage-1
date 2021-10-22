c4b6b109418df14d8b252b5b1c667209d067314b
ribeltran <90879279+ricardobeltranurzua@users.noreply.github.com>
2021-10-22 02:02:01 +0000

    cambios 21102021

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -80,6 +80,6 @@ class ProductosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def producto_params
-      params.require(:producto).permit(:nombre, :categorium_id)
+      params.require(:producto).permit(:nombre, :categorium_id, :descripcion, :imagen)
     end
 end
--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,4 +1,25 @@
 class Producto < ApplicationRecord
   belongs_to :categorium
   
+ validates :nombre,
+  :imagen, :descripcion, presence: { message: "Este campo es requerido."}
+
+  validates :nombre,
+  length: {minimum: 3, maximum: 100, message: "El nombre debe estar entre 3 y 100 caracteres"}
+  
+
+  validates :descripcion,
+  length: {minimum: 15, maximum: 200, message: "El nombre debe estar entre 15 y 200 caracteres"}
+
+  validates :nombre,
+  uniqueness: { message: "Este nombre ya ha sido utilizado."}
+
+  def nproductos
+    return Producto
+      .where(categorium_id: self.id)
+      .count
+  end  
+
 end
+
+
